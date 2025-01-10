32f2676e8c76379b84e286bf95c8249de03a3ba4
thuvominh <thu.vominh23@gmail.com>
2025-01-11 00:39:41 +0700

    [Frontend] - Conflict Jest and import.meta

--- frontend-chatbot/vite.config.js
+++ frontend-chatbot/vite.config.js
@@ -3,11 +3,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
-import EnvironmentPlugin from "vite-plugin-environment";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), EnvironmentPlugin(["VITE_BACKEND_BASE_URL"])],
+  plugins: [react()],
   resolve: {
     alias: {
       '~': path.resolve(__dirname, 'src'),
