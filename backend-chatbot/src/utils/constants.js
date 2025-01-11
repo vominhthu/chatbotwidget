8947f53dcb66812b0a563fce092ebc1bf0652c09
thuvominh <thu.vominh23@gmail.com>
2025-01-11 11:13:37 +0700

    [Backend] Add whitelist domain

--- backend-chatbot/src/utils/constants.js
+++ backend-chatbot/src/utils/constants.js
@@ -1,1 +1,1 @@
-export const WHILTELIST_DOMAINS = ["http://localhost:3000", "http://thuvodev.online", "https://thuvodev.online"]
+export const WHILTELIST_DOMAINS = ["http://localhost:3000", "http://51.79.254.203", "http://thuvodev.online", "https://thuvodev.online"]
